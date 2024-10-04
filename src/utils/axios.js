ef2a6c6b200d9e0a70c6405f6b5f5f1a1a5099bd
Mazrul <mazrul@logistcinfotech.co.in>
2024-10-04 11:09:26 +0530

    url changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    process.env.NEXT_APP_HOST_API || "https://admin.jovialgaming.com/api/v1",
-  //process.env.NEXT_APP_HOST_API || "http://52.32.200.186:1337/api/v1",
+    //process.env.NEXT_APP_HOST_API || "https://admin.jovialgaming.com/api/v1",
+    process.env.NEXT_APP_HOST_API || "http://52.32.200.186:1337/api/v1",
   // process.env.NEXT_APP_HOST_API ||
   // "https://fetish-finder-dev.dryrun.click/api/v1",
   headers: {
