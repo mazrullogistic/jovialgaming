657ffb0877b452a73227a133494c16c1f7cdadcc
AmanullahNiazi <amanullah8502@gmail.com>
2025-04-15 18:09:02 +0500

    Ads-will-be-hidden-if-users-subscribe-a-package

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-  // process.env.NEXT_APP_HOST_API || "https://admin.jovialgaming.com/api/v1", // Need to unable this before deploy on production
+  process.env.NEXT_APP_HOST_API || "https://admin.jovialgaming.com/api/v1", // Need to unable this before deploy on production
   // process.env.NEXT_APP_HOST_API || "https://staging-new.jovialgaming.com:1337//api/v1",
-  process.env.NEXT_APP_HOST_API || "http://localhost:1337/api/v1",
+  // process.env.NEXT_APP_HOST_API || "http://localhost:1337/api/v1",
 
   headers: {
     "Content-Type": "application/json",
