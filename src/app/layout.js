7e8d7f5c7e08e4f65f2c0781997af98dc0e91880
Mazrul <jeel@logisticinfotech.co.in>
2025-07-10 20:00:09 +0530

    Misc changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -95,7 +95,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      {/* google ads script */}
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5381942317128703"
